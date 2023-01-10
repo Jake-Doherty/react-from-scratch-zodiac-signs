80c8fbf1d416266a8322a0e32761b12dad3ce39d
Jacob Doherty <doherty.jacob.p@gmail.com>
2023-01-09 17:33:26 -0800

    create Header Component

---
+++ src/components/Header/Header.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './Header.css';
+
+export default function Header() {
+  return (
+    <header className="App-header">
+      <img src={`/images/logo192.png`} className="App-logo" alt="logo" />
+      <p>Rest of app coming Soon!</p>
+      <a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
+        Learn React
+      </a>
+    </header>
+  );
+}
