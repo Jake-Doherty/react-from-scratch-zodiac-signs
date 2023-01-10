fa43d87d40963efd287580ebbf98e6893aeb323e
Jacob Doherty <doherty.jacob.p@gmail.com>
2023-01-09 19:44:52 -0800

    add more styles and add background

--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -2,12 +2,13 @@ import React from 'react';
 import Zodiac from '../Zodiac/Zodiac';
 import { zodiacs } from '../../data';
 import './Main.css';
+import background from '../../../src/background.jpg';
 
 export default function Main() {
   return (
-    <main>
+    <main style={{ backgroundImage: `url(${background})` }}>
       {zodiacs.map((zodiac) => (
-        <Zodiac key={zodiac.id} name={zodiac.name} type={zodiac.dates} symbol={zodiac.symbol} />
+        <Zodiac key={zodiac.id} name={zodiac.name} dates={zodiac.dates} symbol={zodiac.symbol} />
       ))}
     </main>
   );
